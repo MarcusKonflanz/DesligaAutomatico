192b465c94c9e366e516e540ae5aff7c7e943f74
Marcus Konflanz <47761477+MarcusKonflanz@users.noreply.github.com>
2025-03-19 11:41:26 -0300

    Add files via upload

---
+++ DesligaAutomatico/DesligaAutomatico/FmrDesliga.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DesligaAutomatico
+{
+    public partial class FmrDesliga : Form
+    {
+        public int cancelTimer { get; set; }
+
+        public FmrDesliga()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgendar_Click(object sender, EventArgs e)
+        {
+            cancelTimer = 0;
+            btnAgendar.Visible = false;
+            btnAgendar.Enabled = false;
+            btnCancelar.Visible = true;
+            txtTime.Enabled = false;
+
+            int time = 0;
+            int currentTime = 0;
+            int error = 0;
+
+            try
+            {
+                time = Convert.ToInt32(txtTime.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Insira um número válido para o tempo.");
+                error++;
+            }
+
+            int timeMin = 0;
+            timeMin = time * 60;
+
+            pbTimer.Minimum = 0;
+            pbTimer.Maximum = timeMin;
+            pbTimer.Value = 0;
+
+
+
+            while (currentTime != timeMin)
+            {
+                currentTime++;
+                Thread.Sleep(500);
+                string tempoRestante = Convert.ToString((timeMin - currentTime) / 60);
+                if (tempoRestante == "0")
+                    lblTempoRestante.Text = "o computador será desligado em: 00:" + Convert.ToString(timeMin - currentTime) + " segundos.";
+                else
+                    lblTempoRestante.Text = "o computador será desligado em: " + Convert.ToString(tempoRestante) + " minutos.";
+
+                pbTimer.Value = currentTime;
+                Application.DoEvents(); // Processa eventos da UI
+
+                if (cancelTimer > 0)
+                {
+                    timeMin = 0;
+                    currentTime = 0;
+                    pbTimer.Value = 0;
+                }
+                Thread.Sleep(500);
+            }
+            lblTempoRestante.Visible = false;
+            if (cancelTimer > 0)
+            {
+                btnAgendar.Visible = true;
+                btnAgendar.Enabled = true;
+                btnCancelar.Visible = false;
+                txtTime.Enabled = true;
+            }
+            else
+            {
+                if (error == 0)
+                {
+                    Process.Start("shutdown", "/s /t 0");
+                }
+                else
+                {
+                    btnAgendar.Visible = true;
+                    btnAgendar.Enabled = true;
+                    btnCancelar.Visible = false;
+                    txtTime.Enabled = true;
+                }
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            cancelTimer++;
+        }
+    }
+}
